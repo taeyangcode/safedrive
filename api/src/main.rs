b7eb7131af63fabc90b0cfc11050c4e18e487bfc
taeyangcode <taeyangcode@gmail.com>
2024-02-03 20:06:49 -0800

    add: data points endpoint

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,5 +1,5 @@
 use axum::{extract::Query, response::IntoResponse, routing::get, Router};
-use polars::io::{csv::CsvReader, SerReader};
+use polars::{frame::DataFrame, io::{csv::CsvReader, SerReader}, lazy::{dsl::col, frame::IntoLazy}};
 use serde::{Deserialize, Serialize};
 
 const ACCIDENTS_PATH: &'static str = "./accidents_sample.csv";
@@ -28,18 +28,36 @@ struct Coordinates {
 
 #[derive(Debug, Serialize, Deserialize)]
 struct Bounds {
-    north_east: f32,
-    south_west: f32,
+    north_east: Coordinates,
+    south_west: Coordinates,
 }
 
-fn read_csv(path: &str) {
-    let dataframe = CsvReader::from_path(path).unwrap().finish().unwrap();
+fn csv_to_dataframe(path: &str) -> DataFrame {
+    return CsvReader::from_path(path).unwrap().finish().unwrap();
+}
 
-    println!("{dataframe}");
+#[derive(Debug, Serialize, Deserialize)]
+struct WeightedLocation {
+    coordinates: Coordinates,
+    weight: i32,
+}
+
+fn weighted_location_in_bounds(data_frame: DataFrame, bounds: Bounds) {
+    let Bounds { north_east, south_west } = bounds;
+
+    let weighted_locations = data_frame
+        .lazy()
+        .select(&[col("Start_Lat"), col("Start_Lng"), col("End_Lat"), col("End_Lng"), col("Severity")])
+        .filter(col("Start_Lat").gt_eq(south_west.latitude).and(col("End_Lat").lt_eq(north_east.latitude)))
+        .filter(col("Start_Lng").gt_eq(south_west.longitude).and(col("End_Lng").lt_eq(north_east.longitude)))
+        .collect()
+        .unwrap();
+
+    println!("{weighted_locations}");
 }
 
 async fn data_points(Query(bounds): Query<Bounds>) -> impl IntoResponse {
-    read_csv(ACCIDENTS_PATH);
+    weighted_location_in_bounds(csv_to_dataframe(ACCIDENTS_PATH), bounds);
 
     return "data points"
 }
